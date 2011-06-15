d9ffb813880cd6387ef102310fe6b9bb741efc26
Mark Simpson <verdammelt@gmail.com>
2011-06-14 21:57:39 -0400

    Initial commit of new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thinner::Application.config.secret_token = '3a4c8c74ee4657310d41a2f0a911dab3e01d600a71b6682ae82943cb4d2dc93b909184b9e7d1f3fb79f4bd97758baa522f5450faa47b13833ca6996342d23b2a'
