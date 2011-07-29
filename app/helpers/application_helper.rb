2506dd211689c111581db79341125536e051add4
Mark Simpson <verdammelt@gmail.com>
2011-07-29 18:30:59 -0400

    spike done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,18 @@
 module ApplicationHelper
+  include GoogleVisualization
+
   def round_for_display f
     return f.round(2) unless f.nil?
     nil
+  end
+
+  def weight_chart data
+    visualization "fattrack", "LineChart", :html => {:class => "graph_chart"} do |chart| 
+      chart.date 'Day'
+      chart.number 'Weight'
+      chart.number 'Trend'
+
+      chart.add_rows((@recent_weights.reverse << @today).collect { |w| [w.date, w.weight, round_for_display(w.trend)]}) 
+    end
   end
 end
