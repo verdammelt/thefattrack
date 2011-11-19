2363c2075dbf7fb6697d4ff47ee8f2759f403621
Mark Simpson <verdammelt@gmail.com>
2011-11-18 18:34:47 -0600

    fixing dumb error in earlier commits - tests were not passing\!

--- spec/views/static/about.html.erb_spec.rb
+++ spec/views/static/about.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'static/about.html.erb' do
   it 'exists' do
     render
-    rendered.should contain("They're made out of meat")
+    rendered.should contain("They're made out of... meat")
     rendered.should contain("Ugly bags of mostly water")
   end
 end
